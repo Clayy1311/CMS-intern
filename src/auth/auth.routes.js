3be4cb0373151f0f4bf29fea3ef9d4c2e921115a
Haqi25 <baihaqi1401@student.ub.ac.id>
2025-09-18 14:35:45 +0700

    add_google_login

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { register, login, profile, logout, 
     verifyemail, requestResetPassword, resetpassword } from "./auth.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { google } from "./google.controller.js";
 
 const router = Router();
 
@@ -11,7 +12,7 @@ router.get("/profile", authMiddleware, profile );
 router.post("/logout",  logout);
 router.get("/verifyemail", verifyemail);
 router.post("/requestreset", requestResetPassword);
-router.post("/resetpassword", resetpassword)
-
+router.post("/resetpassword", resetpassword);
+router.post("/google", google);
 
 export default router;
