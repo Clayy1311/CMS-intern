fc2a237dca37681ee5f9259f956f0cd427c2b67a
Haqi25 <baihaqi1401@student.ub.ac.id>
2025-09-22 10:20:49 +0700

    add_userdata_and_home_profile

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import authUser from "./src/auth/auth.routes.js";
+import userData from "./src/datauser/data.user.routes.js";
+import profile from "./src/profile/profile.routes.js";
+import home from "./src/home/home.routes.js"
+
 
 const app = express();
 
@@ -12,6 +16,10 @@ app.get("/", (res) => {
 });
 
 app.use("/api/auth", authUser);
+app.use("/api/g", userData);
+app.use("/api/dashboard", profile, home);
+
+
 const PORT = process.env.PORT || 3000;
 
 
