8b7899ec1ad328dc9d9dd3f83a38bf55630d5618
Haqi25 <baihaqi1401@student.ub.ac.id>
2025-10-20 23:01:14 +0700

    fix prisma client

--- src/routes/home.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { home } from "./home.controller.js";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
-
-const router = Router();
-
-router.get("/home", authMiddleware, home)
-
-export default router;
