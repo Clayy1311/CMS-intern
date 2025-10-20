478638bf53c071f2d0ea9b269c13604892d2c1a0
Haqi25 <baihaqi1401@student.ub.ac.id>
2025-10-20 22:58:19 +0700

    fix import authMiddleware

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import authUser from "./routes/auth.routes";
+import userData from "./routes/data.user.routes";
+import profile from "./routes/profile.routes";
+import home from "./routes/home.routes";
+import { Request, Response } from "express";
+import { authMiddleware } from "./middlewares/auth.middleware";
+
+const app = express();
+
+app.use(express.json());
+
+dotenv.config();
+app.get("/", (req: Request, res:Response) => {
+    res.send("Halo")
+});
+
+app.use("/api/auth", authUser);
+app.use("/api/g", userData);
+app.use("/api/dashboard", authMiddleware, profile, home);
+
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on http://localhost:${PORT}`)
+});
