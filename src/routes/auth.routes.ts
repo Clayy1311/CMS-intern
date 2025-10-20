ae45c12811f3124b7b1adc3b5241f617c6167aab
Haqi25 <baihaqi1401@student.ub.ac.id>
2025-10-20 22:55:33 +0700

    refactor: simplify auth

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { register, login,  logout, 
+    verifyemail, requestResetPassword, resetpassword,refreshToken } from "../controllers/auth.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { google } from "../controllers/google.controller"
+
+
+const router = Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout",  authMiddleware, logout);
+router.get("/verifyemail", verifyemail);
+router.post("/requestreset", requestResetPassword);
+router.post("/resetpassword", resetpassword);
+router.post("/refresh-token", refreshToken);
+router.post("/google", google);
+
+
+export default router;
