75b282587f73b2f643b1448f838492c567071e61
Haqi25 <baihaqi1401@student.ub.ac.id>
2025-10-21 19:10:10 +0700

    fix: import function resetPassword from controller

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { register, login,  logout, 
-    verifyemail, requestResetPassword, resetpassword,refreshToken } from "../controllers/auth.controller";
+    emailVerify, requestResetPassword, resetPassword,refreshToken } from "../controllers/auth.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { google } from "../controllers/google.controller"
 
@@ -10,9 +10,9 @@ const router = Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout",  authMiddleware, logout);
-router.get("/verifyemail", verifyemail);
+router.get("/verifyemail", emailVerify);
 router.post("/requestreset", requestResetPassword);
-router.post("/resetpassword", resetpassword);
+router.post("/resetpassword", resetPassword);
 router.post("/refresh-token", refreshToken);
 router.post("/google", google);
 
